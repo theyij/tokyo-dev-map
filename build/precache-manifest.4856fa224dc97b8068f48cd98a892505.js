d1db759d0e26bcdc9d24004beb8bbb574fba321c
theyij <theyijen@gmail.com>
2019-11-29 23:08:29 +0900

    react app push

---
+++ build/precache-manifest.4856fa224dc97b8068f48cd98a892505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cded9626e94e563c237500dc0f0711b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56e98331592933813c16",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "97eddc900b57112b72eb",
+    "url": "/static/js/2.8708ef5c.chunk.js"
+  },
+  {
+    "revision": "56e98331592933813c16",
+    "url": "/static/js/main.529d8b77.chunk.js"
+  },
+  {
+    "revision": "38f9964958070d5bde00",
+    "url": "/static/js/runtime-main.ef2d3f17.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
+
+function header() {
+    const header = (
+    <div class="header">
+        <h1>This will be our react coffee map app</h1>
+        <p>It is {new Date().toLocaleTimeString()}.</p>
+    </div>);
+    ReactDOM.render(header, document.getElementById('header'));
+}
+function input() {
+    const input = (
+    <div class="input">
+        <h2>Input stuff is here</h2>
+    </div>);
+    ReactDOM.render(input, document.getElementById('input'));
+}
+function list() {
+    const list = (
+    <div class="list">
+        <h2>list is here</h2>
+    </div>);
+    ReactDOM.render(list, document.getElementById('list'));
+}
+function map() {
+    const map = (
+    <div class="map">
+        <h2>google map is here</h2>
+    </div>);
+    ReactDOM.render(map, document.getElementById('map'));
+}
+header();
+input();
+list();
+map();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
