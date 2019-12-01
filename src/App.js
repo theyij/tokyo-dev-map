11408476357951638c232cc9aa67ee95a3b56134
Bryenne Kay <bryenne.k@gmail.com>
2019-12-01 19:46:15 +0900

    none of this works yet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Login from './pages/LogIn';
+import Signup from './pages/SignUp';
+import PrivateRoute from './PrivateRoute';
+import { AuthContext } from "./context/auth";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
+    return (
+      <AuthContext.Provider value={true}>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home Page</Link>
+              </li>
+              <li>
+                <Link to="/admin">Admin Page</Link>
+              </li>
+            </ul>
+
+            { /* the routes go here here here */}
+            <Route exact path="/second" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <PrivateRoute path="/admin" component={Admin} />
+          </div>
+        </Router>
+      </AuthContext.Provider>
+
   );
 }
 
 export default App;
+
