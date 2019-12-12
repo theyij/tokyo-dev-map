8bdd524595392707e6ef1d2094cb86dba7d7b0c6
Bryenne Kay <bryenne.k@gmail.com>
2019-12-12 23:07:45 +0900

    trying to organize routes

--- src/components/Header/routes.js
+++ src/components/Header/routes.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import './routes.css';
+import Add from '../Add/add.js';
 
 const HomePage = () => <div>This is a Home Page</div>
+const ProfilePage = () => <div>This is the Profile Page</div>
 const LoginPage = () => <div>This is a Login Page</div>
 const SignupPage = () => <div>This is a Signup Page</div>
-const ProfilePage = () => <div>This is the Profile Page</div>
-const AddPage = () => <div>This is an Add Page</div>
+const AddPage = () => <div>add page here</div>
 const EditPage = () => <div>This is a Edit Page</div>
 const DeletePage = () => <div>This is a Delete Page</div>
 
 export default function Routes() {
   return(
-    <div>
+    <div class="routes">
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/components/profile'>Profile</Link></li>
+        <li><Link to='/profile'>Profile</Link></li>
         <li><Link to='/login'>Login</Link></li>
         <li><Link to='/signup'>Signup</Link></li>
         <li><Link to='/add'>Add</Link></li>
@@ -22,17 +24,16 @@ export default function Routes() {
         <li><Link to='/delete'>Delete</Link></li>
       </ul>
 
-      <div className="container">
-        <Route path="/" exact component={HomePage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/profile" component="{ProfilePages}" />
         <Route path="/login" component={LoginPage} />
         <Route path="/signup" component="{SignupPage}" />
         <Route path="/add" component="{AddPage}" />
         <Route path="/edit" component="{EditPage}" />
         <Route path="/delete" component="{DeletePage}" />
-        <Route path="./profile" component="{ProfilePages}" />
-      </div>
+      </Switch>
     </div>
-
   );
 }
 
